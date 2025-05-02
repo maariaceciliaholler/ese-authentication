fb5e48135103b363dbfa43cce45f4b94e161e9e3
luizanurnberg <nurnbergluiza@gmail.com>
2025-05-02 19:33:04 -0300

    refactor(folders): removed unused folders

--- src/constants/payment-status.const.ts
+++
@@ -1,6 +0,0 @@
-export const PAYMENT_STATUS = {
-    APPROVED: "Aprovado",
-    DISAPPROVED: "Reprovado",
-} as const;
-
-export type TPaymentStatus = (typeof PAYMENT_STATUS)[keyof typeof PAYMENT_STATUS];
